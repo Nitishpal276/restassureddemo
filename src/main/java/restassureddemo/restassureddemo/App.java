0751b013720c7c2b0243afc3d63b061be42787a8
Nitish Pal <nitishpal276@gmail.com>
2025-06-29 20:52:48 +0530

    Initial commit

---
+++ src/main/java/restassureddemo/restassureddemo/App.java
@@ -0,0 +1,13 @@
+package restassureddemo.restassureddemo;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
